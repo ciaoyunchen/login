5a8307f318b1067c6d5ee854f68e83621243217d
iamdoris1993 <dolly.chen702@gmail.com>
2019-10-31 09:14:38 +0800

    20191031-會員登入及註冊系統

--- login_api.php
+++ login_api.php
@@ -18,17 +18,43 @@ echo "pw=".$pw;
 $dsn="mysql:host=localhost;charset=utf8;dbname=mydb";
 $pdo=new PDO($dsn,'root','iamdoris19930303');
 
-$sql="select * from user where acc='$acc' && pw='$pw'";
+// for A & B
+// $sql="select * from user where acc='$acc' && pw='$pw'";
+$sql="select count(*) as 'r' from user where acc='$acc' && pw='$pw'";
 
-$data= $pdo->query($sql)->fetch();
+// for A & B & C
+// fetch() 回傳陣列  fetchColumn() 回傳true或false/1或0
+// $data= $pdo->query($sql)->fetch();
+$data= $pdo->query($sql)->fetchColumn();
 
 print_r($data);
 
-if($acc==$data['acc'] && $pw==$data['pw']){
+// A累贅的判斷式
+// if($acc==$data['acc'] && $pw==$data['pw']){
+//   echo "登入成功";
+// }else{
+//   echo "登入失敗";
+// }
+
+// B浪費的判斷式(不管資料大小只會回傳有無值))
+// if(!empty($data)){
+//   echo "登入成功";
+// }else{
+//   echo "登入失敗";
+// }
+
+// C簡單判斷式
+// if($data['r']==1){
+//   echo "登入成功";
+// }else{
+//   echo "登入失敗";
+// }
+
+// D最簡單判斷式(判斷true或false/1或0)
+if($data){
   echo "登入成功";
 }else{
   echo "登入失敗";
 }
-
 
 ?>
